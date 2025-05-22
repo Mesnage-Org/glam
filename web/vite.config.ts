22becf501bdac7da163a0d08f9316d1244230542
Brooks J Rady <b.j.rady@gmail.com>
2025-05-22 20:31:41 +0100

    fix(build): add sveltekit to web workers

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 	worker: {
+		plugins: () => [sveltekit()],
 		format: 'es'
 	},
 	esbuild: {

0b8396f575a0853cb86eab389cfcaf145a44c2b1
Brooks J Rady <b.j.rady@gmail.com>
2025-05-29 13:45:41 +0100

    fix(web): fix `glycowork` loading in Pyodide
    By default, `glycowork` depends on `glycorender[png]`, but that pulls in
    `pymupdf` which doesn't play nice with Pyodide. Manually loading
    `glycorender` without `[png]` removes that problematic dependency.

--- web/src/lib/glam.ts
+++ web/src/lib/glam.ts
@@ -23,7 +23,8 @@ const pyodide = await loadPyodide({
 await pyodide.loadPackage(['micropip']);
 const micropip = pyodide.pyimport('micropip');
 
-await micropip.install(`${base}/theglam-1.2.0-py3-none-any.whl`);
+await micropip.install(['glycorender==0.2.0', `${base}/theglam-1.2.0-py3-none-any.whl`]);
+
 await pyodide.runPythonAsync('from glam import *');
 const generate_glycopeptides = pyodide.globals.get('generate_glycopeptides');
 
